b1cad7502242d070409f1306cc4259e4b67e9705
GauravWalia19 <gauravwalia019@gmail.com>
2021-01-17 23:45:17 +0530

    added water mark in initializing the project

--- .erboilerplate/config.js
+++ .erboilerplate/config.js
@@ -20,7 +20,14 @@ module.exports = {
         return false;
     },
     init: ()=>{
-        console.log('\x1b[1;31m',`Application Running in ${process.env.NODE_ENV}`,'\x1b[0m');
+        console.log('\x1b[1;5;34m',' _____       _____       _____  ');
+        console.log('\x1b[1;5;34m','| ____|     |  _  |     |  _  | ');
+        console.log('\x1b[1;5;34m','| |_        | |_| |     | |_| | ');
+        console.log('\x1b[1;5;34m','|  _|       | ____|     |  _  / ');
+        console.log('\x1b[1;5;34m','| |___   _  |   \\    _  | |_| \\ ')
+        console.log('\x1b[1;5;34m','|_____| |_| |_|\\_\\  |_| |_____| ');
+        console.log('\x1b[0m');
+        console.log('\x1b[1;93m',`Application Running in ${process.env.NODE_ENV}`,'\x1b[0m');
     },
     getIconsPath: ()=>{
         const dirname = __dirname.split("/");
